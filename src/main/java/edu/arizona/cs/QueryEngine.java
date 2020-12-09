45174e9277e4352cd79dfd4be371a9682625a46e
Joseph Acevedo <joseph.acevedo@outlook.com>
2020-12-09 15:20:22 -0700

    Set constants to a default value

--- src/main/java/edu/arizona/cs/QueryEngine.java
+++ src/main/java/edu/arizona/cs/QueryEngine.java
@@ -1,5 +1,7 @@
 package edu.arizona.cs;
 
+import org.tartarus.snowball.ext.PorterStemmer;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +20,7 @@ public class QueryEngine {
 
         int qaCount = 0;
         while (qaScanner.hasNextLine()) {
-            String line = qaScanner.nextLine().toLowerCase().trim();
+            String line = qaScanner.nextLine().trim();
             if (line.isEmpty())
                 continue;
 
@@ -32,15 +34,12 @@ public class QueryEngine {
                     qaCount++;
                     break;
                 case 2:
-                    String[] answers = line.split("\\|");
+                    String[] answers = line.toLowerCase().split("\\|");
                     currQuestion.answers = new String[answers.length];
                     for (int i = 0; i < answers.length; i++) {
                         currQuestion.answers[i] = answers[i];
                     }
-                    System.out.printf("Question: %s\nAnswers:\n", currQuestion.question);
-                    for (String answer : currQuestion.answers) {
-                        System.out.println("\t"+answer);
-                    }
+
                     qaCount = 0;
                     questions.add(currQuestion);
                     currQuestion = new Question();
@@ -50,6 +49,7 @@ public class QueryEngine {
     }
 
     public float testQuestions(WikiParser wiki) throws IOException {
+        int numCorrect = 0;
         BufferedWriter writer = new BufferedWriter(new FileWriter(ANSWERS_OUTPUT_FILEPATH));
         float mmr = 0;
         for (Question q : this.questions) {
@@ -61,34 +61,57 @@ public class QueryEngine {
                 ResultClass result = results.get(i);
                 for (String answer : q.answers) {
                     if (result.doc.get("title").equalsIgnoreCase(answer)) {
-                        writer.write(String.format("%d. *%s*\n", i + 1, result.doc.get("title")));
+                        writer.write(String.format("%d. *%s* (%.3f)\n", i + 1, result.doc.get("title"), result.score));
+                        if (i == 0)
+                            numCorrect++;
                         mmr += (1.0/(i + 1));
                         correct = true;
                         break;
                     }
                 }
                 if (!correct) {
-                    writer.write(String.format("%d.  %s \n", i + 1, result.doc.get("title")));
+                    writer.write(String.format("%d.  %s (%.3f)\n", i + 1, result.doc.get("title"), result.score));
                 } else {
                     break;
                 }
             }
         }
+        System.out.printf("Answered %d/%d correct\n", numCorrect, this.questions.size());
         writer.close();
         mmr /= questions.size();
         return mmr;
     }
 
+    /**
+     * Tests the performance of the system when using different boost factors for modifying queries.
+     *
+     * @param wiki The system to modify
+     * @throws IOException If the output file DNE
+     */
+    private void testBoostFactor(WikiParser wiki) throws IOException {
+        float minBoost = 0.0f;
+        float maxBoost = 2.0f;
+        float boostStep = 0.1f;
+
+        for (float currBoost = minBoost; currBoost < maxBoost; currBoost += boostStep) {
+            WikiParser.BOOST_FACTOR = currBoost;
+            System.out.printf("(%.1f, %.5f)\n", currBoost, testQuestions(wiki));
+        }
+    }
+
     public static void main(String[] args) {
-        WikiParser wiki = new WikiParser("C:\\Users\\Joseph\\Desktop\\wiki-subset-20140602", true);
+        // Loads the index. If an index already exists it will load the index based on constants for including stop words and lemmatizing
+        boolean buildIndex = false;
+        WikiParser wiki = new WikiParser("C:\\Users\\Joseph\\Desktop\\wiki-subset-20140602", !buildIndex);
         QueryEngine qe = new QueryEngine();
 
         try {
             wiki.buildIndex();
             Scanner scanner = new Scanner(System.in);
             qe.createAnswers();
+            // qe.testBoostFactor(wiki);
             float mmr = qe.testQuestions(wiki);
-            System.out.println("MMR Score: " + mmr);
+            System.out.println("MRR Score: " + mmr);
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/edu/arizona/cs/WikiParser.java
+++ src/main/java/edu/arizona/cs/WikiParser.java
@@ -1,5 +1,8 @@
 package edu.arizona.cs;
 
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.CoreDocument;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.simple.Sentence;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.core.StopAnalyzer;
@@ -31,6 +34,7 @@ import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.Scanner;
 
 public class WikiParser {
@@ -41,7 +45,10 @@ public class WikiParser {
     private Analyzer analyzer;
     private Directory index;
     private boolean indexExists;
-    private static final boolean LEMMATIZE = false;
+    private static final boolean STOP_WORDS = true;
+    private static final boolean LEMMATIZE = true;
+    private static final boolean POS_BOOST = true;
+    public static float BOOST_FACTOR = 1.1f;
 
     public WikiParser(String inputDirectory, boolean indexExists) {
         this.inputDirectory = inputDirectory;
@@ -49,12 +56,19 @@ public class WikiParser {
     }
 
     public void buildIndex() throws IOException {
+        long timeStart = System.currentTimeMillis();
         this.stemmer = new PorterStemmer();
-        this.analyzer = new StopAnalyzer(Paths.get(STOP_WORDS_FILEPATH)); //ptrstm_nsw_index
+        this.analyzer = new StandardAnalyzer();//StopAnalyzer(Paths.get(STOP_WORDS_FILEPATH));
         if (LEMMATIZE) {
-            this.index = FSDirectory.open(Paths.get(String.format("%s\\cnlp_sw_index", this.inputDirectory)));
+            if (STOP_WORDS)
+                this.index = FSDirectory.open(Paths.get(String.format("%s\\cnlp_sw_index", this.inputDirectory)));
+            else
+                this.index = FSDirectory.open(Paths.get(String.format("%s\\cnlp_nsw_index", this.inputDirectory)));
         } else {
-            this.index = FSDirectory.open(Paths.get(String.format("%s\\ptrstm_nsw_index", this.inputDirectory)));
+            if (STOP_WORDS)
+                this.index = FSDirectory.open(Paths.get(String.format("%s\\ptrstm_sw_index", this.inputDirectory)));
+            else
+                this.index = FSDirectory.open(Paths.get(String.format("%s\\ptrstm_nsw_index", this.inputDirectory)));
         }
         if (indexExists)
             return;
@@ -91,26 +105,17 @@ public class WikiParser {
                     currDoc.add(new StringField("title", line, Field.Store.YES));
                 } else if (line.startsWith("==") && line.endsWith("==")) {
                     // ==section title==
-                    // for now treat the same as normal text
-                    String[] tokens = line.split("[\\.,\\s!;?:\"]+");
-                    for (String token : tokens) {
-                        stemmer.setCurrent(token);
-                        stemmer.stem();
-                        currDoc.add(new TextField("section", stemmer.getCurrent(), Field.Store.YES));
+                    if (LEMMATIZE) {
+                        addLemmaToDoc(currDoc, "section", line);
+                    } else {
+                        addStemToDoc(currDoc, "section", line);
                     }
                 } else {
                     // line in wiki page
-                    // using porter stemmer for stemming
-                    try {
-                        String[] tokens = line.split("[\\.,\\s!;?:\"]+");
-                        for (String token : tokens) {
-                            stemmer.setCurrent(token);
-                            stemmer.stem();
-                            currDoc.add(new TextField("text", stemmer.getCurrent(), Field.Store.YES));
-                        }
-                    } catch (Exception e) {
-                        System.out.println("Error on line: " + line);
-                        e.printStackTrace();
+                    if (LEMMATIZE) {
+                        addLemmaToDoc(currDoc, "text", line);
+                    } else {
+                        addStemToDoc(currDoc, "text", line);
                     }
                 }
             }
@@ -118,20 +123,68 @@ public class WikiParser {
         }
         writer.close();
         this.indexExists = true;
+        System.out.println("Building collection took: " + (System.currentTimeMillis() - timeStart)/1000/60 + " minutes");
+    }
+
+    private void addLemmaToDoc(Document doc, String key, String sentence) {
+        try {
+            Sentence sent = new Sentence(sentence);
+            for (String lemma : sent.lemmas()) {
+                doc.add(new TextField(key, lemma, Field.Store.YES));
+            }
+        } catch (Exception e) {
+            System.out.println("Error on line: " + sentence);
+            e.printStackTrace();
+        }
+    }
+
+    private void addStemToDoc(Document doc, String key, String sentence) {
+        try {
+            String[] tokens = sentence.split("[\\.,\\s!;?:\"]+");
+            for (String token : tokens) {
+                stemmer.setCurrent(token);
+                stemmer.stem();
+                doc.add(new TextField(key, stemmer.getCurrent(), Field.Store.YES));
+            }
+        } catch (Exception e) {
+            System.out.println("Error on line: " + sentence);
+            e.printStackTrace();
+        }
     }
 
     public List<ResultClass> answerQuestion(String query) throws IOException {
         Query q = null;
         try {
+            List<String> tags = getPropNouns(query);
+            query = query.toLowerCase();
+
             if (LEMMATIZE) {
                 Sentence sent = new Sentence(query);
                 query = String.join(" ", sent.lemmas());
             } else {
-                stemmer.setCurrent(query);
-                stemmer.stem();
-                query = stemmer.getCurrent();
+                String[] tokens = query.split("[\\.,\\s!;?:\"]+");
+                query = "";
+                for (String token : tokens) {
+                    stemmer.setCurrent(token);
+                    stemmer.stem();
+                    query += stemmer.getCurrent() + " ";
+                }
             }
-            q = new QueryParser("text", analyzer).parse(QueryParser.escape(query));
+
+            query = QueryParser.escape(query);
+            if (POS_BOOST) {
+                int prevTermIdx = 0;
+                for (String noun : tags) {
+                    int idx = query.indexOf(noun, prevTermIdx);
+                    if (idx == -1) {
+                        continue;
+                    }
+                    prevTermIdx = idx + 1;
+                    query = query.substring(0, idx + noun.length()) + "^" + BOOST_FACTOR + query.substring(idx + noun.length());
+                }
+            }
+
+            q = new QueryParser("text", analyzer).parse(query);
         } catch (ParseException e) {
             e.printStackTrace();
         }
@@ -149,10 +202,23 @@ public class WikiParser {
             Document d = searcher.doc(docId);
 
             ResultClass result = new ResultClass(d, hits[i].score);
-            System.out.printf("%d. [%s](%.5f)\n", i+1, d.get("title"), hits[i].score);
+            // System.out.printf("%d. [%s](%.5f)\n", i+1, d.get("title"), hits[i].score);
             results.add(result);
         }
         return results;
     }
 
+    private List<String> getPropNouns(String line) {
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize,ssplit,pos");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+        CoreDocument doc = pipeline.processToCoreDocument(line);
+        List<String> tags = new ArrayList();
+        for (CoreLabel tok : doc.tokens()) {
+            if (tok.tag().equals("NNP") || tok.tag().equals("NNPS")) {
+                tags.add(tok.word().toLowerCase());
+            }
+        }
+        return tags;
+    }
 }
